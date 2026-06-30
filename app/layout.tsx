659c6c8fd48f447977fd2d433becb5075670517e
DanielAlmeida19 <dani1114vivilinda@gmail.com>
2026-06-30 13:58:47 -0300

    feat: add test navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,19 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <nav>
+          <Image
+            src="/CorridOteca.png"
+            alt="Descrição da imagem para acessibilidade"
+            width={500}
+            height={300}
+          />
+          <div>Alguma coisa</div>
+          <div>Alguma coisa</div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
